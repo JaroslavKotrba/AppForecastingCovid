93d05d403f2ec6be2566731003dc1f90fb3d43c3
JaroslavKotrba <65077222+JaroslavKotrba@users.noreply.github.com>
2021-08-05 16:03:34 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,6 +8,8 @@ library(forecast)
 library(dplyr)
 library(plotly)
 
+data <- readRDS(file="country.2020.rds")
+
 # ui
 ui <- dashboardPage(
   skin='red',
@@ -35,7 +37,56 @@ ui <- dashboardPage(
       inputId = "country",
       multiple = TRUE,
       label = strong("Select Countries to Compare:", style = "font-family: 'arial', font-size: 14px"),
-      choices = sort(unique(data$country_name)),
+      choices = c("Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", 
+                  "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", 
+                  "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", 
+                  "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", 
+                  "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegovina", 
+                  "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", 
+                  "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso", 
+                  "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Caribbean Netherlands", 
+                  "Cayman Islands", "Central African Republic", "Chad", "Chile", 
+                  "China", "Christmas Island", "Cocos Islands", "Colombia", "Comoros", 
+                  "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cura?ao", "Cyprus", 
+                  "Czech Republic", "Democratic Republic of the Congo", "Denmark", 
+                  "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", 
+                  "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", 
+                  "Ethiopia", "Falkland Islands", "Faroe Islands", "Fiji", "Finland", 
+                  "France", "French Guiana", "French Polynesia", "French Southern Territories", 
+                  "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", 
+                  "Greece", "Greenland", "Grenada", "Guam", "Guatemala", "Guernsey", 
+                  "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard Island and McDonald Islands", 
+                  "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", 
+                  "Iran", "Iraq", "Ireland", "Isle of Man", "Israel", "Italy", 
+                  "Ivory Coast", "Jamaica", "Japan", "Jersey", "Jordan", "Kazakhstan", 
+                  "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", 
+                  "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", 
+                  "Lithuania", "Luxembourg", "Macau", "Macedonia", "Madagascar", 
+                  "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", 
+                  "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", 
+                  "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", 
+                  "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", 
+                  "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", 
+                  "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", 
+                  "North Korea", "Northern Mariana Islands", "Norway", "Oman", 
+                  "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", 
+                  "Paraguay", "Peru", "Philippines", "Pitcairn Islands", "Poland", 
+                  "Portugal", "Puerto Rico", "Qatar", "Republic of the Congo", 
+                  "R?union", "Romania", "Russia", "Rwanda", "Saint Helena", "Saint Kitts and Nevis", 
+                  "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", 
+                  "S?o Tom? and Pr?ncipe", "Saudi Arabia", "Senegal", "Serbia", 
+                  "Seychelles", "Sierra Leone", "Singapore", "Sint Maarten", "Slovakia", 
+                  "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", 
+                  "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", 
+                  "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden", 
+                  "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", 
+                  "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", 
+                  "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", 
+                  "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", 
+                  "United States Minor Outlying Islands", "United States of America", 
+                  "United States Virgin Islands", "Uruguay", "Uzbekistan", "Vanuatu", 
+                  "Vatican City", "Venezuela", "Vietnam", "Wallis and Futuna", 
+                  "Western Sahara", "Yemen", "Zambia", "Zimbabwe"),
       selected = c("Czech Republic", "Austria", "Slovakia")
     ),
     
@@ -108,7 +159,7 @@ ui <- dashboardPage(
         style="padding-top: 0px",
         div(class="jumbotron", style = "background-color: #FFFFFF", div(class = "container", h2("About the Model", code("ARIMA")), p("ARIMA is an acronym that stands for AutoRegressive Integrated Moving Average. It is a class of model that captures a suite of different standard temporal structures in time series data."),
                                                                         p("In statistics and econometrics, and in particular in time series analysis, an autoregressive integrated moving average model is a generalization of an autoregressive moving average model. Both of these models are fitted to time series data either to better understand the data or to predict future points in the series.")
-        )
+          )
         )
       )
     )
